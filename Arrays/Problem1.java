7cd997b1eac6a9c1328c74cbd56de04add38a8ca
Vinod <113439253+Vinodpatil31@users.noreply.github.com>
2022-11-13 18:08:46 +0530

    Add files via upload

---
+++ Arrays/Problem1.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+class Problem1{
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of array : ");
+		int n = sc.nextInt(); // input size of array 
+		int[] arr = new int[n];
+
+		System.out.println("Enter the elements of the array: ");
+		for(int i=0; i<n; i++)
+			arr[i] = sc.nextInt();
+		
+		System.out.print("Input array : ");
+
+		for(int x : arr)
+			System.out.print(x + " ");
+		
+		System.out.println();
+		
+	}
+}
